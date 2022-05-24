971b93de1475531d58728ed2aaf1fc46bab58f65
illustrayking <illustrayking@outlook.com>
2022-05-24 15:37:15 +0430

    solve add up number function

--- add-up-number/index.js
+++ add-up-number/index.js
@@ -1,26 +1,21 @@
 // Add up the Numbers from a Single Number
-
 // Create a function that takes a number as an argument. 
 // Add up all the numbers from 1 to the number you passed to 
 // the function. For example, if the input is 4 then your function 
 // should return 10 because 1 + 2 + 3 + 4 = 10.
-
 //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
-const input = document.querySelector('.js-input') as HTMLInputElement;
-const output = document.querySelector('.js-result') as HTMLDivElement;
-const butn = document.querySelector('.js-button');
-const add_up = (val:string):number => {
-    let num = 0;
-
-    for(let i = 1; i <= +val; i++) {
+var input = document.querySelector('.js-input');
+var output = document.querySelector('.js-result');
+var butn = document.querySelector('.js-button');
+var add_up = function (val) {
+    var num = 0;
+    for (var i = 1; i <= +val; i++) {
         num += i;
     }
     return num;
-}
-
-function result():void {
-    let inputValue = input.value;
+};
+function result() {
+    var inputValue = input.value;
     output.textContent = String(add_up(inputValue));
 }
-butn?.addEventListener('click', result);
+butn === null || butn === void 0 ? void 0 : butn.addEventListener('click', result);
